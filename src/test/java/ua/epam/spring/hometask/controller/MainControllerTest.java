dc8d7dbabff2418ce6d935dcfb32dc8304c16206
Vitalii <vitaliilatysh@ukr.net>
2020-03-16 19:38:31 +0200

    Added MainControllerTest

--- src/test/java/ua/epam/spring/hometask/controller/MainControllerTest.java
+++ src/test/java/ua/epam/spring/hometask/controller/MainControllerTest.java
@@ -1,38 +1,51 @@
 package ua.epam.spring.hometask.controller;
 
-import config.TestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebAppConfiguration
-@ContextConfiguration(classes = TestConfig.class)
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class MainControllerTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     @Test
-    public void createEmployeeAPI() throws Exception {
-        MockMultipartFile jsonFile = new MockMultipartFile("users.json", "", "application/json", "{\"firstName\": \"Donald\", \"lastName\": \"Donawan\"}".getBytes());
+    public void sendFile() throws Exception {
+        Path path = Paths.get("/src/main/resources/files/users.json");
+        String name = "users.json";
+        String originalFileName = "users.json";
+        String contentType = "application/json";
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (final IOException e) {
+        }
+        MockMultipartFile result = new MockMultipartFile(name,
+                originalFileName, contentType, content);
 
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/files")
-                .file(jsonFile)
-                .param("some-random", "4"))
-                .andExpect(status().is(200))
+        mockMvc.perform(multipart("/files")
+                .file(result)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isOk())
                 .andExpect(content().string("success"));
     }
 }
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,22 @@
+drop table if exists User, Company, Phone;
+
+create table User (
+    id              int             auto_increment,
+    first_name       varchar(50)     not null,
+    last_name        varchar(50)     not null
+);
+
+create table Company (
+    id              int             auto_increment,
+    name            varchar(50)     not null
+);
+
+create table Phone (
+    id              int             auto_increment,
+    number          varchar(13)     not null,
+    user_id         int             not null,
+    company_id      int             not null,
+    foreign key (user_id)           references User(id)            on delete cascade,
+    foreign key (company_id)        references Company(id)         on delete cascade,
+    UNIQUE(number)
+);
